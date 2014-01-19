17669c2c5153f717c5bb1c42941da3a66dcf3f72
Pablo Mario <pablomario@bytelchus.com>
2014-01-19 16:38:49 +0100

    Mejorado Knight Rider
    Mejroado agregado programas

--- Knight Rider/Coche_fantastico/Coche_fantastico.ino
+++ Knight Rider/Coche_fantastico/Coche_fantastico.ino
@@ -1,30 +1,91 @@
-int pulsador=10;
-int estadoPulsador=0;
+int estado=0;
 int leds[6] = {1,2,3,4,5,6};
 int estadoLed=0;
-int pulsado=0;
 int led=0;
+int programa = 0;
+int anterior=0;
 
 void setup(){
-  pinMode(pulsador,INPUT);
+  pinMode(10,INPUT);
   for(led=0;led<6;led++){
     pinMode(leds[led],OUTPUT);
   }
 }
 
 void loop(){
-  estadoPulsador=digitalRead(pulsador);
-  if(estadoPulsador==HIGH && pulsado==0){
-    for(led=0;led<=5;led++){
-      digitalWrite(leds[led],HIGH);
+  estado=digitalRead(10);
+   if(estado == HIGH){ 
+     programa++;
+     delay(20);
+   }
+   if(programa==1 && anterior == 0){
+     programa1();
+     anterior = 1;
+   }else if(programa==2 && anterior == 1){
+     programa2();
+     anterior = 2;
+   }else if(programa == 3 && anterior == 2){
+     programa3();
+     anterior=3;
+   }else if(programa == 4 && anterior ==3){
+     programa4();
+     programa=0;
+     anterior=0;
+   }
+ 
+}
+
+void programa1(){
+  for(int i=0; i<5;i++){
+      for(led=0;led<=5;led++){
+        digitalWrite(leds[led],HIGH);
+        delay(50);
+        digitalWrite(leds[led],LOW);
+      }
+      for(led=5;led>=0;led--){
+        digitalWrite(leds[led],HIGH);
+        delay(50);
+        digitalWrite(leds[led],LOW);
+      }
+    }      
+}
+
+void programa2(){
+   for(int j = 0; j<6; j++){
+      digitalWrite(leds[j],HIGH);
       delay(50);
-      digitalWrite(leds[led],LOW);
-    }
-    for(led=5;led>=0;led--){
-      digitalWrite(leds[led],HIGH);
+   }
+}
+
+void programa3(){
+  for(int j = 0; j<6; j++){
+      digitalWrite(leds[j],LOW);
       delay(50);
-      digitalWrite(leds[led],LOW);
     }
-  }
-  pulsado=estadoPulsador;
+ }
+ 
+ 
+void programa4(){
+  for(int j =0; j<3;j++){
+      for(int i=1; i<3;i++){
+        digitalWrite(1,HIGH);
+        digitalWrite(2,HIGH);
+        digitalWrite(3,HIGH);
+        delay(50);
+        digitalWrite(1,LOW);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        delay(50); 
+      }
+      for(int i=1; i<3;i++){
+        digitalWrite(4,HIGH);
+        digitalWrite(5,HIGH);
+        digitalWrite(6,HIGH);
+        delay(50);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        delay(50); 
+      }
+   }  
 }
