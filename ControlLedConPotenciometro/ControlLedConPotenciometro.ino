5e2c689a72b6dc57261ece9729cc32890c37d59c
Pablo Mario <pablomario@bytelchus.com>
2014-01-24 23:56:07 +0100

    Cambios ControlLedPotenciometro
    cambios de pines

--- ControlLedConPotenciometro/ControlLedConPotenciometro.ino
+++ ControlLedConPotenciometro/ControlLedConPotenciometro.ino
@@ -1,6 +1,6 @@
 /* Version Final*/
 
-int leds[10]={0,1,2,4,5,6,7,8,9,10};
+int leds[10]={2,3,4,6,7,8,9,10,11,12};
 int const potenciometro = 0;
 int entradaPotenciometro=0;
 int cacho = 1023/10;
@@ -9,31 +9,34 @@ int posicion = 0;
 void setup(){
   for(int i = 0; i<10;i++){
     pinMode(leds[i],OUTPUT);
-  }
+    digitalWrite(leds[i],LOW);
+  }  
+  Serial.begin(115200);
 }
 
 void loop(){
   entradaPotenciometro = analogRead(potenciometro);
   posicion = entradaPotenciometro/cacho;
-  if(posicion>=1)digitalWrite(0,HIGH);
-  else digitalWrite(0,LOW);
-  if(posicion>=2)digitalWrite(1,HIGH);
-  else digitalWrite(1,LOW);
-  if(posicion>=3)digitalWrite(2,HIGH);
+  Serial.println(posicion); 
+  if(posicion>=1)digitalWrite(2,HIGH);
   else digitalWrite(2,LOW);
-  if(posicion>=4)digitalWrite(4,HIGH);
+  if(posicion>=2)digitalWrite(3,HIGH);
+  else digitalWrite(3,LOW);
+  if(posicion>=3)digitalWrite(4,HIGH);
   else digitalWrite(4,LOW);
-  if(posicion>=5)digitalWrite(5,HIGH);
-  else digitalWrite(5,LOW);
-  if(posicion>=6)digitalWrite(6,HIGH);
+  if(posicion>=4)digitalWrite(6,HIGH);
   else digitalWrite(6,LOW);
-  if(posicion>=7)digitalWrite(7,HIGH);
+  if(posicion>=5)digitalWrite(7,HIGH);
   else digitalWrite(7,LOW);
-  if(posicion>=8)digitalWrite(8,HIGH);
+  if(posicion>=6)digitalWrite(8,HIGH);
   else digitalWrite(8,LOW);
-  if(posicion>=9)digitalWrite(9,HIGH);
+  if(posicion>=7)digitalWrite(9,HIGH);
   else digitalWrite(9,LOW);
-  if(posicion>=10)digitalWrite(10,HIGH);
+  if(posicion>=8)digitalWrite(10,HIGH);
   else digitalWrite(10,LOW);
+  if(posicion>=9)digitalWrite(11,HIGH);
+  else digitalWrite(11,LOW);
+  if(posicion>=10)digitalWrite(12,HIGH);
+  else digitalWrite(12,LOW);
   
 }
